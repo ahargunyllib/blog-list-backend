b9db8e950e04b114aa37daf5a6ba5ad7eec2cbbb
ahargunyllib <billy.bpm03@gmail.com>
2023-12-24 01:16:01 +0700

    initital commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es2021": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
